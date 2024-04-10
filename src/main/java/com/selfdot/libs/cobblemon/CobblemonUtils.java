0a1ea75c57c0c07131f2d523a0023f96a0ac97f3
David Sadler <djs21@ic.ac.uk>
2024-04-10 21:21:17 +0100

    Add statVitaminItem

--- src/main/java/com/selfdot/libs/cobblemon/CobblemonUtils.java
+++ src/main/java/com/selfdot/libs/cobblemon/CobblemonUtils.java
@@ -1,15 +1,23 @@
 package com.selfdot.libs.cobblemon;
 
+import com.cobblemon.mod.common.CobblemonItems;
 import com.cobblemon.mod.common.api.moves.Move;
 import com.cobblemon.mod.common.api.moves.MoveSet;
+import com.cobblemon.mod.common.api.moves.categories.DamageCategories;
 import com.cobblemon.mod.common.api.pokemon.stats.Stats;
+import com.cobblemon.mod.common.api.types.ElementalType;
+import com.cobblemon.mod.common.api.types.ElementalTypes;
 import com.cobblemon.mod.common.item.PokemonItem;
 import com.cobblemon.mod.common.pokemon.EVs;
 import com.cobblemon.mod.common.pokemon.IVs;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.cobblemon.mod.common.pokemon.Species;
 import com.selfdot.libs.minecraft.screen.ItemStackBuilder;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 import static com.selfdot.libs.minecraft.screen.ItemStackBuilder.itemStack;
 import static net.minecraft.util.Formatting.*;
@@ -85,6 +93,63 @@ public class CobblemonUtils {
             );
         }
         return itemStackBuilder;
+    }
+
+    public static String typeString(ElementalType type) {
+        Formatting colour = Formatting.WHITE;
+        if      (type.equals(ElementalTypes.INSTANCE.getGROUND()))   colour = GOLD;
+        else if (type.equals(ElementalTypes.INSTANCE.getROCK()))     colour = GOLD;
+        else if (type.equals(ElementalTypes.INSTANCE.getFIGHTING())) colour = GOLD;
+        else if (type.equals(ElementalTypes.INSTANCE.getPOISON()))   colour = Formatting.DARK_PURPLE;
+        else if (type.equals(ElementalTypes.INSTANCE.getGHOST()))    colour = Formatting.DARK_PURPLE;
+        else if (type.equals(ElementalTypes.INSTANCE.getDRAGON()))   colour = Formatting.DARK_PURPLE;
+        else if (type.equals(ElementalTypes.INSTANCE.getICE()))      colour = Formatting.AQUA;
+        else if (type.equals(ElementalTypes.INSTANCE.getFLYING()))   colour = Formatting.AQUA;
+        else if (type.equals(ElementalTypes.INSTANCE.getGRASS()))    colour = Formatting.GREEN;
+        else if (type.equals(ElementalTypes.INSTANCE.getBUG()))      colour = Formatting.GREEN;
+        else if (type.equals(ElementalTypes.INSTANCE.getFIRE()))     colour = Formatting.RED;
+        else if (type.equals(ElementalTypes.INSTANCE.getELECTRIC())) colour = Formatting.YELLOW;
+        else if (type.equals(ElementalTypes.INSTANCE.getPSYCHIC()))  colour = Formatting.LIGHT_PURPLE;
+        else if (type.equals(ElementalTypes.INSTANCE.getSTEEL()))    colour = Formatting.GRAY;
+        else if (type.equals(ElementalTypes.INSTANCE.getWATER()))    colour = Formatting.BLUE;
+        else if (type.equals(ElementalTypes.INSTANCE.getDARK()))     colour = Formatting.DARK_PURPLE;
+        return colour + type.getDisplayName().getString();
+    }
+
+    public static ItemStackBuilder moveItem(Move move) {
+        boolean isStatus = false;
+        Item item;
+        String damageCategory;
+        if (move.getDamageCategory().getName().equals(DamageCategories.INSTANCE.getPHYSICAL().getName())) {
+            item = Items.MUSIC_DISC_BLOCKS;
+            damageCategory = Formatting.RED + "Physical";
+        } else if (move.getDamageCategory().getName().equals(DamageCategories.INSTANCE.getSPECIAL().getName())) {
+            item = Items.MUSIC_DISC_MALL;
+            damageCategory = Formatting.LIGHT_PURPLE + "Special";
+        } else {
+            item = Items.MUSIC_DISC_STRAD;
+            damageCategory = Formatting.GRAY + "Status";
+            isStatus = true;
+        }
+
+        ItemStackBuilder itemStackBuilder = itemStack(item)
+            .withName(move.getDisplayName().getString())
+            .withLore(typeString(move.getType()))
+            .withLore(damageCategory);
+        if (!isStatus) itemStackBuilder.withLore(GOLD + "Power: " + (int)move.getPower());
+        return itemStackBuilder;
+    }
+
+    public static ItemStackBuilder statVitaminItem(Stats stat) {
+        return itemStack(switch (stat) {
+            case HP -> CobblemonItems.HP_UP;
+            case ATTACK -> CobblemonItems.PROTEIN;
+            case DEFENCE -> CobblemonItems.IRON;
+            case SPECIAL_ATTACK -> CobblemonItems.CALCIUM;
+            case SPECIAL_DEFENCE -> CobblemonItems.ZINC;
+            case SPEED -> CobblemonItems.CARBOS;
+            default -> CobblemonItems.CHARCOAL;
+        }).withName(stat.getDisplayName().getString());
     }
 
 }
