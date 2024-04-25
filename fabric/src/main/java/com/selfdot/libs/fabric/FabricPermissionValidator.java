b0692114e8d8a91b7d283d66ff9d872b5264f123
David Sadler <djs21@ic.ac.uk>
2024-04-25 02:52:30 +0100

    Move FabricPermissionValidator to fabric package

--- fabric/src/main/java/com/selfdot/libs/fabric/FabricPermissionValidator.java
+++ fabric/src/main/java/com/selfdot/libs/fabric/FabricPermissionValidator.java
@@ -1,5 +1,7 @@
-package com.selfdot.libs.minecraft.permissions;
+package com.selfdot.libs.fabric;
 
+import com.selfdot.libs.minecraft.permissions.Permission;
+import com.selfdot.libs.minecraft.permissions.PermissionValidator;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.command.CommandSource;
 
