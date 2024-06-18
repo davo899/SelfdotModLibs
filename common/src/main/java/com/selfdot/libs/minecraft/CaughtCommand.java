1f086cee7b1fe939fce060192caaa5057c5205fb
David Sadler <djs21@ic.ac.uk>
2024-06-18 23:47:06 +0100

    Make CommandSyntaxExceptions not be caught

--- common/src/main/java/com/selfdot/libs/minecraft/CaughtCommand.java
+++ common/src/main/java/com/selfdot/libs/minecraft/CaughtCommand.java
@@ -2,6 +2,7 @@ package com.selfdot.libs.minecraft;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import lombok.extern.slf4j.Slf4j;
 import net.minecraft.server.command.ServerCommandSource;
 
@@ -10,12 +11,14 @@ import java.util.Arrays;
 @Slf4j
 public abstract class CaughtCommand implements Command<ServerCommandSource> {
 
-    protected abstract int execute(CommandContext<ServerCommandSource> context);
+    protected abstract int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException;
 
     @Override
-    public int run(CommandContext<ServerCommandSource> context) {
+    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         try {
             return execute(context);
+        } catch (CommandSyntaxException e) {
+            throw e;
         } catch (Exception e) {
             e.printStackTrace();
         } catch (Throwable t) {
