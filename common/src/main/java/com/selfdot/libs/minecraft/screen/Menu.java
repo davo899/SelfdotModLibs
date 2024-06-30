3cfe6163fe1baba07158225c0a523c63c0b9a3ab
David Sadler <djs21@ic.ac.uk>
2024-07-01 00:49:09 +0100

    Add pagedViewFactory method to Menu

--- common/src/main/java/com/selfdot/libs/minecraft/screen/Menu.java
+++ common/src/main/java/com/selfdot/libs/minecraft/screen/Menu.java
@@ -1,16 +1,22 @@
 package com.selfdot.libs.minecraft.screen;
 
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
+import eu.pb4.sgui.api.elements.GuiElementBuilderInterface;
 import eu.pb4.sgui.api.gui.SimpleGuiBuilder;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import static com.selfdot.libs.minecraft.screen.ScreenUtils.relativePosition;
 import static net.minecraft.item.Items.BARRIER;
 import static net.minecraft.util.Formatting.RED;
 
-public class Menu {
+public abstract class Menu {
 
     private final ScreenHandlerType<?> screenHandlerType;
     protected final ServerPlayerEntity player;
@@ -20,13 +26,22 @@ public class Menu {
         this.player = player;
     }
 
-    private void openView(ViewFactory viewFactory) {
+    protected <U> PagedViewFactory<U> pagedViewFactory(
+        Supplier<List<U>> elementsSupplier,
+        Function<U, GuiElementBuilderInterface<?>> iconFactory,
+        Consumer<U> onElementClick,
+        ViewFactory viewFactory
+    ) {
+        return new PagedViewFactory<>(this::openView, elementsSupplier, iconFactory, onElementClick, viewFactory);
+    }
+
+    protected void openView(ViewFactory viewFactory) {
         SimpleGuiBuilder guiBuilder = new SimpleGuiBuilder(screenHandlerType, false);
         viewFactory.create(guiBuilder);
         guiBuilder.build(player).open();
     }
 
-    private void returnsTo(SimpleGuiBuilder guiBuilder, ViewFactory viewFactory) {
+    protected void returnsTo(SimpleGuiBuilder guiBuilder, ViewFactory viewFactory) {
         guiBuilder.setSlot(
             relativePosition(guiBuilder, 1 / 2f, 1),
             new GuiElementBuilder()
