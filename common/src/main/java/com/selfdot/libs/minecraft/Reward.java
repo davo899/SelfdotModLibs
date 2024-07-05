b9ec1960874d90f28858fb8f11f87a585bc40467
David Sadler <djs21@ic.ac.uk>
2024-07-05 13:04:37 +0100

    Add Reward class

---
+++ common/src/main/java/com/selfdot/libs/minecraft/Reward.java
@@ -0,0 +1,15 @@
+package com.selfdot.libs.minecraft;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.util.List;
+
+import static com.selfdot.libs.minecraft.CommandExecutionBuilder.execute;
+
+public record Reward(String displayName, List<String> commandList) {
+
+    public void give(ServerPlayerEntity player) {
+        commandList.forEach(command -> execute(command).withPlayer(player).as(player.getServer()));
+    }
+
+}
