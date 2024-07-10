a159c96d8155fc07bb38978766a9e26ceaef4e89
David Sadler <djs21@ic.ac.uk>
2024-07-10 13:24:26 +0100

    Add item editing to PartyViewFactory

--- common/src/main/java/com/selfdot/libs/cobblemon/PartyViewFactory.java
+++ common/src/main/java/com/selfdot/libs/cobblemon/PartyViewFactory.java
@@ -4,6 +4,7 @@ import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.api.storage.NoPokemonStoreException;
 import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
 import com.cobblemon.mod.common.pokemon.Pokemon;
+import com.selfdot.libs.minecraft.screen.ItemElementBuilder;
 import com.selfdot.libs.minecraft.screen.ViewFactory;
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
 import eu.pb4.sgui.api.gui.SimpleGuiBuilder;
@@ -21,24 +22,27 @@ public class PartyViewFactory extends ViewFactory {
 
     private final ServerPlayerEntity player;
     private final Consumer<Pokemon> onClick;
+    private final Consumer<ItemElementBuilder<?>> editItem;
 
     public PartyViewFactory(
-        ServerPlayerEntity player, Consumer<Pokemon> onClick, Consumer<SimpleGuiBuilder> create
+        ServerPlayerEntity player, Consumer<Pokemon> onClick,
+        Consumer<ItemElementBuilder<?>> editItem, Consumer<SimpleGuiBuilder> create
     ) {
         super(ScreenHandlerType.GENERIC_9X3, create);
         this.player = player;
         this.onClick = onClick;
+        this.editItem = editItem;
     }
 
     @Override
     protected void build(SimpleGuiBuilder guiBuilder) {
+        super.build(guiBuilder);
         PlayerPartyStore party;
         try {
             party = Cobblemon.INSTANCE.getStorage().getParty(player.getUuid());
         } catch (NoPokemonStoreException e) {
             return;
         }
-
         for (int i = 0; i < party.size(); i++) {
             Pokemon pokemon = party.get(i);
             int x = 1 + i + (i / 3);
@@ -51,13 +55,11 @@ public class PartyViewFactory extends ViewFactory {
                 );
                 continue;
             }
-            guiBuilder.setSlot(
-                absolutePosition(guiBuilder, x, 2),
-                new PokemonElementBuilder(pokemon.getSpecies(), pokemon.getAspects())
-                    .setCallback(() -> onClick.accept(pokemon))
-            );
+            ItemElementBuilder<?> item = new PokemonElementBuilder(pokemon.getSpecies(), pokemon.getAspects())
+                .setCallback(() -> onClick.accept(pokemon));
+            editItem.accept(item);
+            guiBuilder.setSlot(absolutePosition(guiBuilder, x, 2), item);
         }
-        super.build(guiBuilder);
     }
 
 }
