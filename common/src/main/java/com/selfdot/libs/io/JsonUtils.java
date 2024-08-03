854fd069747f7bfa7b5fc7860e536d689830625a
David Sadler <djs21@ic.ac.uk>
2024-08-03 10:41:43 +0100

    Add JsonUtils.save

--- common/src/main/java/com/selfdot/libs/io/JsonUtils.java
+++ common/src/main/java/com/selfdot/libs/io/JsonUtils.java
@@ -21,7 +21,20 @@ public class JsonUtils {
             if (!Files.exists(path)) Files.createDirectories(path);
 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public static <T> void save(Gson gson, T object, String filename) {
+        createDirectories(Path.of(filename).getParent());
+        try (FileWriter writer = new FileWriter(filename, StandardCharsets.UTF_8)) {
+            gson.toJson(object, writer);
+            writer.flush();
+            log.info("Saved {}", filename);
+
+        } catch (IOException e) {
+            log.error("Could not save {}", filename);
+            e.printStackTrace(System.err);
         }
     }
 
@@ -31,25 +44,16 @@ public class JsonUtils {
         createDirectories(Path.of(filename).getParent());
         try (FileReader reader = new FileReader(filename, StandardCharsets.UTF_8)) {
             object = typeDef.fromJson(gson, reader);
-            log.info("Loaded " + filename);
+            log.info("Loaded {}", filename);
         } catch (FileNotFoundException e) {
-            log.warn(filename + " not found");
+            log.warn("{} not found", filename);
         } catch (IOException | JsonSyntaxException e) {
             shouldSave = false;
-            log.error("Could not load " + filename);
-            e.printStackTrace();
+            log.error("Could not load {}", filename);
+            e.printStackTrace(System.err);
         }
-        if (shouldSave) {
-            try (FileWriter writer = new FileWriter(filename, StandardCharsets.UTF_8)) {
-                gson.toJson(object, writer);
-                writer.flush();
-                log.info("Saved " + filename);
-            } catch (IOException e2) {
-                log.error("Could not save " + filename);
-                e2.printStackTrace();
-            }
-        }
-        if (object == default_) log.warn("Using default object as substitute for " + filename);
+        if (shouldSave) save(gson, object, filename);
+        if (object == default_) log.warn("Using default object as substitute for {}", filename);
         return object;
     }
 
