30d6d0f8584f01ee2c93af0ceb7d02c2abb6ef92
David Sadler <djs21@ic.ac.uk>
2024-08-05 09:57:40 +0100

    Add Reward.giveAndTell

--- common/src/main/java/com/selfdot/libs/minecraft/Reward.java
+++ common/src/main/java/com/selfdot/libs/minecraft/Reward.java
@@ -1,15 +1,22 @@
 package com.selfdot.libs.minecraft;
 
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 import java.util.List;
 
+import static com.selfdot.libs.minecraft.MinecraftUtils.colourize;
 import static com.selfdot.libs.minecraft.command.CommandExecutionBuilder.execute;
 
 public record Reward(String displayName, List<String> commandList) {
 
     public void give(ServerPlayerEntity player) {
         commandList.forEach(command -> execute(command).withPlayer(player).as(player.getServer()));
+    }
+
+    public void giveAndTell(ServerPlayerEntity player, String message) {
+        give(player);
+        player.sendMessage(Text.literal(colourize(message.replace("%reward%", displayName))));
     }
 
 }
